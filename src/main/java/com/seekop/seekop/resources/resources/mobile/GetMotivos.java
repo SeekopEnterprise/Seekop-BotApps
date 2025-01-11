fa6c34702d365ad8c300b9b80dbb06de5d9c610c
Fabián Quintero <52643984+poemi1988@users.noreply.github.com>
2025-01-10 19:02:35 -0600

    SOUX-8829-servicio-motivos-rechazo
    servicio-motivos-rechazo

---
+++ src/main/java/com/seekop/seekop/resources/resources/mobile/GetMotivos.java
@@ -0,0 +1,126 @@
+package com.seekop.seekop.resources.resources.mobile;
+
+import resources.CommonSeekopUtilities;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import org.json.JSONException;
+import org.json.JSONObject;
+import resources.ConnectionManager;
+
+/**
+ *
+ * @author Seekop
+ */
+public class GetMotivos extends CommonSeekopUtilities {
+
+    private boolean produccion = true;
+    private String descripcionServicio = "Obtencion de motivos rechazo valuacion";
+    private String fechainicio = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(Calendar.getInstance().getTime());
+    private String fechafin = "";
+
+    private String ip = "";
+    private String token = "";
+    private String idMarca = "";
+    private String idAuto = "";
+    private String idProducto = "";
+    private String idModelo = "";
+    private String tipo = "";
+    //////
+    private String json = "";
+    private String jsonMandado = "";
+    private String jsonBody = " ";
+    private String dbsicop = "";
+    private String auxdbsicop = "sicopdb";
+    //////
+
+    public GetMotivos(String recibidoJSON, String ip) {
+        jsonMandado = recibidoJSON;
+        if (recibidoJSON != null && !"".equals(recibidoJSON)) {
+            recibidoJSON = recibidoJSON.toUpperCase();
+            JSONObject objetoJson;
+            try {
+                objetoJson = new JSONObject(recibidoJSON.toUpperCase());
+                this.ip = ip;
+                this.token = objetoJson.getString("TOKEN");
+            } catch (JSONException ex) {
+                setErrorMensaje("JSON malformed: " + ex.toString());
+            }
+            if (!token.isEmpty()) {
+                getTokenInformation(token);
+                if (getConnectionDistribuidor() != null) {
+                    conection();
+                } else {
+                    setErrorMensaje("No se encontro una conexion para el TOKEN='" + token + "'");
+                }
+            } else {
+                setErrorMensaje("el 'TOKEN' es necesario para esta operacion y no debe estar vacio");
+            }
+
+        } else {
+            setErrorMensaje("Not JSON fount");
+        }
+        json = generaJSONRespuesta();
+        CloseConnection();
+    }
+
+    private void conection() {
+        String baseSeminuevos = getNombreSeminuevos(getIdDistribuidor());
+        AbrirConnectionSeminuevos();
+        String sql = "SELECT \n"
+                    + "    IdTipoDeDeclinacion, Nombre\n"
+                    + "FROM\n"
+                    + "" + baseSeminuevos + ".tipodedeclinacionauto\n"
+                    + "WHERE Activo <> 0\n";
+
+            ConnectionManager connectionSeminuevos = abrirConnection(getIdDistribuidor());
+            if (connectionSeminuevos.executeQuery(sql)) {
+                while (connectionSeminuevos.next()) {
+                    if (!validarvacio(connectionSeminuevos.getString("Nombre"), "").equals("")) {
+                        jsonBody += "\n{\n"
+                                + "    \"IdMotivo\": \"" + connectionSeminuevos.getString("IdTipoDeDeclinacion") + "\",\n"
+                                + "    \"nombreMotivo\": \"" + validarvacio(connectionSeminuevos.getString("Nombre"), "").replace("\"", "") + "\"\n"
+                                + "},";
+                    }
+                }
+            }
+            
+        jsonBody = jsonBody.substring(0, jsonBody.length() - 1);
+    }
+
+    private String generaJSONRespuesta() {
+        String respuesta = "";
+        switch (getStatus()) {
+            case 0:
+                respuesta = "{\n"
+                        + "    \"codigo\": \"" + getStatus() + "\",\n"
+                        + "    \"mensaje\": \"" + getMensaje() + "\",\n"
+                        + "    \"motivos\": []\n"
+                        + "}";
+                break;
+            case 1:
+                respuesta = "{\n"
+                        + "    \"codigo\": \"" + getStatus() + "\",\n"
+                        + "    \"mensaje\": \"OK\",\n"
+                        + "    \"motivos\": [" + jsonBody + "]\n"
+                        + "}";
+                break;
+        }
+        return respuesta;
+    }
+
+    private void cargarRequestsApi() {
+        fechafin = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(Calendar.getInstance().getTime());
+        String bitacora = "INSERT INTO sicopdb.requestsapi\n"
+                + "(IdInterfaz, IdDistribuidor, Servicio, Operacion, Api, FechaInicio, FechaFin, Ip, Response, Request) \n"
+                + "VALUES ('MX00000SKP', '" + token + "', '" + descripcionServicio + "', 'POST', 'REST', '" + fechainicio + "', '" + fechafin + "', '" + ip + "', '" + json.replaceAll("'", "").replaceAll("\r", "").replaceAll("\n", "").replaceAll("\t", "") + "', '" + jsonMandado.replaceAll("'", "").replaceAll("\r", "").replaceAll("\n", "").replaceAll("\t", "") + "');";
+        if (getConnectionATI().execute(bitacora, false)) {
+        } else {
+            setErrorMensaje(getConnectionATI().getErrorMessage());
+        }
+    }
+
+    public String getJson() {
+        return json;
+    }
+
+}
